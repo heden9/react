363001e086e0da9e1d3fab0cbb92e7760877a014
mangX <771854332@qq.com>
2017-04-18 20:28:46 +0800

    day5 照片墙初步版

---
+++ react -test/photo.js
@@ -0,0 +1,6 @@
+var PhotoInfo = [
+    {"Url":"../img/m1.jpg","Des":"People_1","Inv":"People_12"},
+    {"Url":"../img/m2.jpg","Des":"People_2","Inv":"People_22"},
+    {"Url":"../img/m3.jpg","Des":"People_3","Inv":"People_32"},
+    {"Url":"../img/m4.jpg","Des":"People_4","Inv":"People_42"}
+];
